434a47663c9162017346ca7806cc47239dec4b7c
Zoe Webb <zwebb3@gmu.edu>
2022-11-01 22:11:18 -0400

    Initial

---
+++ Package setup.R
@@ -0,0 +1,33 @@
+#install packages
+install.packages(
+  c("tidyverse", 
+    "tidyselect", 
+    "cowplot", 
+    "googleway", 
+    "ggplot2", 
+    "ggrepel", 
+    "ggspatial", 
+    "lwgeom", 
+    "sf", 
+    "rnaturalearth",
+    "rnaturalearthdata", 
+    "ggthemes", 
+    "jpeg", 
+    "png")
+)
+
+#load packages
+library(tidyverse)
+library(tidyselect)
+library(cowplot)
+library(googleway)
+library(ggplot2)
+library(ggrepel)
+library(ggspatial)
+library(lwgeom)
+library(sf)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(ggthemes)
+library(jpeg)
+library(png)
